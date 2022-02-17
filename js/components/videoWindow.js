3604e48ae1127fd5d4f5f716ab414a64927e8723
Paulius <p.ziedelis@gmail.com>
2022-02-17 17:49:43 +0200

    changed js for video

---
+++ js/components/videoWindow.js
@@ -0,0 +1,7 @@
+// function video() {
+//     let popup = document.getElementById('vidPopup');
+//     popup.classList.toggle('show');}
+// function videoClose() {
+//     let popup = document.getElementById('vidPopup');
+//     popup.classList.toggle('hide');}
+
--- js/pages/home.js
+++ js/pages/home.js
@@ -1,5 +1,5 @@
 // IMPORTS
-
+// import videoWindow from './components/videoWindow';
 // CODE EXECUTION BELOW THIS COMMENT LINE
 
 /* header start */
@@ -12,12 +12,43 @@
 /* about block 1 end */
 
 /* about block 2 start */
-function video() {
-    let popup = document.getElementById('vidPopup');
-    popup.classList.toggle('show');}
-function videoClose() {
-    let popup = document.getElementById('vidPopup');
-    popup.classList.toggle('hide');}
+function video(id, src) {
+    let popup = document.getElementById(id);
+    popup.classList.toggle('show');
+    let a = document.getElementById('videoAbout2');
+    if (a === null) {
+        let iframe = document.createElement('iframe');
+        // iframe.classList.add("vid");
+        iframe.classList.add('vid');
+        iframe.id = 'videoAbout2';
+        iframe.src = src;
+        //  + '?rel=0';
+        iframe.title = null;
+        let element = document.getElementById(id);
+        element.appendChild(iframe);
+    }
+    // let iframe = document.createElement('iframe');
+    // iframe.classList.add("vid");
+    // iframe.classList.add("video1");
+    // iframe.id='videoAbout2';
+    // iframe.src=src;
+    // let element = document.getElementById(id);
+    // element.appendChild(iframe);
+}
+
+function videoClose(id) {
+    let popup = document.getElementById(id);
+    let elmnt = document.getElementById(id);
+    elmnt.remove();
+    popup.classList.toggle('hide');
+}
+
+// function remove(){
+//     const text = document.querySelectorAll('video1');
+//     for (const el of text) {
+//         el.remove();
+//     }
+// }
 /* about block 2 end */
 
 /* counter block start */
