f374076d4e5a093002369a797e08d46860f11fe6
Laura <97021990+lauraMeiz@users.noreply.github.com>
2022-02-23 13:30:51 +0200

    test commit

--- js/components/services/services.js
+++ js/components/services/services.js
@@ -15,12 +15,79 @@ function services(selector, data) {
     }
 
     let HTML = '';
+    const isValidServiceObject = (service) => {
+        if (
+            typeof service !== 'object' ||
+            service === null ||
+            Array.isArray(service)
+        ) {
+            return false;
+        }
+
+        /*const keys = Object.keys(service);
+        if (keys.length < 3 || keys.length > 4) {
+            return false;
+        }*/
+        return true;
+    };
+
+    const isValidServiceImg = (img) => {
+        if (typeof img !== 'string' || img === '') {
+            return false;
+        }
+        const parts = img.split('.');
+        if (
+            parts.length < 2 ||
+            parts[0].trimStart() === '' ||
+            parts[1].trim() === ''
+        ) {
+            return false;
+        }
+        return true;
+    };
+
+    const isValidServiceAlt = (alt) => {
+        if (typeof alt !== 'string') {
+            return false;
+        }
+        return true;
+    };
+
+    const isValidServiceTitle = (h3) => {
+        if (typeof h3 !== 'string') {
+            return false;
+        }
+        return true;
+    };
+
+    const isValidServiceDescription = (description) => {
+        if (typeof description !== 'string') {
+            return false;
+        }
+        return true;
+    };
+
+    const isValidService = (service) => {
+        if (
+            !isValidServiceObject(service) ||
+            !isValidServiceImg(service.img) ||
+            !isValidServiceAlt(service.alt) ||
+            !isValidServiceTitle(service.h3) ||
+            !isValidServiceDescription(service.description)
+        ) {
+            return false;
+        }
+        return true;
+    };
+
     for (const service of data) {
-        if (typeof service !== 'object') {
+        if (!isValidService(service)) {
             continue;
         }
         HTML += `<div class="short-about service">
-        <img class="icon-services" src="./img/icon/img-icon-visible/${service.img}.png"alt="${service.alt}" />
+        <img class="icon-services" src="./img/icon/img-icon-visible/${service.img.trim()}"alt="${
+            service.alt
+        }" />
         <div class="short-about-title ">
         <h3>${service.h3}</h3>
         </div>
@@ -31,7 +98,9 @@ function services(selector, data) {
          </div>
       </div>`;
     }
-
+    if (HTML === '') {
+        return [true, 'Data has no valid service objects'];
+    }
     servicesDOM.innerHTML = HTML;
 
     return [false, 'OK'];
--- js/data/servicesData.js
+++ js/data/servicesData.js
@@ -1,41 +1,41 @@
 const servicesData = [
     {
-        img: 'brain',
+        img: 'brain.png',
         alt: 'services',
         h3: 'Virtual Personal Assistants',
         description:
             'Capitalize on low hanging fruit to identify a ball park value added activity to beta test. Override the digital divide with additional',
     },
     {
-        img: 'twohead',
+        img: 'twohead.png',
         alt: 'services',
         h3: 'Videos Surveillance',
         description:
             'Capitalize on low hanging fruit to identify a ball park value added activity to beta test. Override the digital divide with additional',
     },
     {
-        img: 'brain2',
+        img: 'brain2.png',
         alt: 'services',
         h3: 'Social Media Services',
         description:
             'Capitalize on low hanging fruit to identify a ball park value added activity to beta test. Override the digital divide with additional',
     },
     {
-        img: 'brain3',
+        img: 'brain3.png',
         alt: 'services',
         h3: 'Online Customer Support',
         description:
             'Capitalize on low hanging fruit to identify a ball park value added activity to beta test. Override the digital divide with additional',
     },
     {
-        img: 'head',
+        img: 'head.png',
         alt: 'services',
         h3: 'Search Engine Result Refining',
         description:
             'Capitalize on low hanging fruit to identify a ball park value added activity to beta test. Override the digital divide with additional',
     },
     {
-        img: 'brain4',
+        img: 'brain4.png',
         alt: 'services',
         h3: 'Online Fraud Detection',
         description:
